29621fb846b01fde74bc0850f06d6cfdbf3ec2cb
Piyush Itankar <pitankar@gmail.com>
2015-11-11 07:02:49 +0530

    Added: + File Operations Table + OPEN, READ, WRITE, RELEASE functions. * Implementation of file operations function call to be done

--- pit.c
+++ pit.c
@@ -1,46 +1,60 @@
-/*
+/******************************************
  * Author: Piyush Itankar
  * Module: Demo Character Drive
  * Date:   10th Nov' 2015
- */
+ ******************************************/
 
-#include<linux/kernel.h>
-#include<linux/module.h>
-#include<linux/cdev.h>
-#include<linux/fs.h>
-#include<linux/slab.h>
-#include<linux/uaccess.h>
 #include "pit.h"
+
+// Open
+static int pit_open(struct inode *inode, struct file *filp){
+	//TODO
+	return 0;
+}
+
+// Read
+static ssize_t pit_read(struct file *inode, char *buff, size_t count, loff_t *off){
+	//TODO
+	return 0;
+}
+
+// Write
+static ssize_t pit_write (struct file *inode, const char *buff, size_t count, loff_t *off){
+	//TODO
+	return 0;
+}
+
+// Release
+static int pit_release(struct inode *inode, struct file *filp){
+	//TODO
+	return 0;
+}
 
 // Init module
 static int _init_(void){
-    
     // Get a Major Number
     if ( 0 == alloc_chrdev_region( &pit_dev, 0, 1, "pit") )
-      printk(KERN_ALERT "__ pit is here __ [ MAJOR:%d ]", MAJOR(pit_dev));
+      printk( KERN_ALERT "__ pit is here __ [ MAJOR:%d ]", MAJOR(pit_dev) );
     else
-      printk(KERN_ALERT "Failed to Register Device");
+      printk( KERN_ALERT "Failed to Register Device" );
     
-    pit_dev_area = kmalloc(sizeof(struct dev_area), GFP_KERNEL);
-    memset(pit_dev_area, 0, sizeof(struct dev_area));
+    pit_dev_area = kmalloc( sizeof(struct dev_area), GFP_KERNEL );
+    memset( pit_dev_area, 0, sizeof(struct dev_area) );
 
     pit_dev_area->pit_cdev.owner = THIS_MODULE;
     return 0;
-
 }
 
 // Exit Module
-void _exit_(void){
- 
-    unregister_chrdev_region(pit_dev,1);
+void _exit_( void ){
+    unregister_chrdev_region( pit_dev, 1 );
     kfree( pit_dev_area );
-    printk(KERN_ALERT " __pit was here__ ");
-
+    printk( KERN_ALERT " __pit was here__ " );
 }
 
-module_init(_init_);
-module_exit(_exit_);
+module_init( _init_ );
+module_exit( _exit_ );
 
-MODULE_LICENSE("GPL");
-MODULE_AUTHOR("Piyush Itankar, pitankar@gmail.com");
-
+MODULE_LICENSE( "GPL" );
+MODULE_AUTHOR( "Piyush Itankar, <pitankar@gmail.com>" );
+MODULE_DESCRIPTION( "Simple Character Driver" );
--- pit.h
+++ pit.h
@@ -1,12 +1,47 @@
-/*
+/******************************************
  * Author: Piyush Itankar
  * Module: Demo Character Drive
  * Date:   10th Nov' 2015
- */
+ ******************************************/
 
+#include<linux/kernel.h>
+#include<linux/module.h>
+#include<linux/cdev.h>
+#include<linux/fs.h>
+#include<linux/slab.h>
+#include<linux/uaccess.h>
+
+/*
+ * File operations Functions Prototypes
+ */
+static int pit_open(struct inode *, struct file *);
+static ssize_t pit_read(struct file *, char *, size_t, loff_t *);
+static ssize_t pit_write (struct file *, const char *, size_t, loff_t *);
+static int pit_release(struct inode *, struct file *);
+
+/*
+ * Saves the Major and Minor number
+ */
 static dev_t pit_dev = 0;
 
+/*
+ * Devce structure. cbuff is on byte memory space
+ * would be used to read and write a byte.
+ */
 struct dev_area{
   char cbuff;
   struct cdev pit_cdev;
 } *pit_dev_area;
+
+/*
+ * File operations table for the device
+ * Only OPEN, READ, WRITE and CLOSE calls to 
+ * be supported so far
+ */
+struct file_operations pit_fops = {
+	owner:   THIS_MODULE,
+	open:    pit_open,
+	read:    pit_read,
+	write:   pit_write,
+	release: pit_release
+};
