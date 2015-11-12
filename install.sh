47e3aadd5760794be36ec486ba47c8ba7d31df4c
Piyush Itankar <pitankar@gmail.com>
2015-11-12 10:19:19 +0530

    Cleaned up the install.sh a little bit. + Keeps the directory clean. ! Driver Major and Minor number are still hard coded. Need to fix that ASAP

--- install.sh
+++ install.sh
@@ -10,21 +10,47 @@ then
   exit
 fi
 
+#Remove the module
+if [ $1 = "remove" ]
+then 
+  rmmod pit
+  if [ $? -ne 0 ]
+  then
+    echo "Module not present. Cannot Unload. Exitting."
+    exit
+  fi
+  rm /dev/pit
+  echo "Module Removed from Kernel. Device /dev/pit is removed."
+  
+  # Keep the directory Clean
+  make clean
+  
+  exit
+fi
+
+# Clean Modules
+make clean
+
+# Create Modules
+make all
+
+# Things went wrong. Report
+if [ $? -ne 0 ]
+then
+  echo "Make Failed"
+  exit
+fi
+
 #Add the module
 if [ $1 = "add" ]
 then
   insmod pit.ko
   mknod /dev/pit c 244 0
   echo "Module Pushed into the Kernel. Device Path /dev/pit"
-  exit
-fi
-
-#Remove the module
-if [ $1 = "remove" ]
-then 
-  rmmod pit
-  rm /dev/pit
-  echo "Module Removed from Kernel. Device /dev/pit is removed."
+  
+  # Keep the directory Clean
+  make clean
+  
   exit
 fi
 
